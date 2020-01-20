65f4322e0ded7ecbd73f632d656c0d7027196a37
Samuel Simões <samuelsimoes31@gmail.com>
2020-01-20 15:50:33 -0300

    Ler strings da serial

--- lpc1768_tests/src/main.cpp
+++ lpc1768_tests/src/main.cpp
@@ -3,8 +3,11 @@
 Serial pc(USBTX, USBRX); // tx, rx
 
 void rxInterrupt(){
-  pc.putc('a');
-  if(pc.readable())  pc.putc(pc.getc());
+  //pc.putc('a');
+  char str[6];
+  if(pc.readable())  pc.gets(str,5);
+  pc.puts(str);
+  str[0] = 0;
 }
 
 int main()
